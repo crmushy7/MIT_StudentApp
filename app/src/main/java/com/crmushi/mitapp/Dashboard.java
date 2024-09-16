6c372c3699b95e3b5727ee761409269830fbb7c0
crmushy7 <gidionmushy23@gmail.com>
2024-09-16 11:41:13 +0300

    updated

--- app/src/main/java/com/crmushi/mitapp/Dashboard.java
+++ app/src/main/java/com/crmushi/mitapp/Dashboard.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -17,14 +18,18 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import Adapters.CoursesAdapter;
@@ -80,13 +85,42 @@ public class Dashboard extends AppCompatActivity {
                 builder.setView(popupView);
                 course_description_dialog = builder.create();
                 course_description_dialog.show();
-                Button upload=popupView.findViewById(R.id.button_request);
+                Button requestBtn=popupView.findViewById(R.id.button_request);
                 TextView courseName=popupView.findViewById(R.id.coursename);
                 TextView courseDuration=popupView.findViewById(R.id.courseduration);
                 TextView courseDescription=popupView.findViewById(R.id.coursedescription);
                 courseName.setText(itemSetGet.getCourseName());
                 courseDuration.setText("Duration: "+itemSetGet.getCourseDuration());
                 courseDescription.setText(itemSetGet.getCourseDescription());
+                requestBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Calendar calendar = Calendar.getInstance();
+                        String currentdate = DateFormat.getInstance().format(calendar.getTime());
+                        HashMap<Object,String> hashMap=new HashMap<>();
+                        hashMap.put("Student Name","name");
+                        hashMap.put("StudentID","name");
+                        hashMap.put("Course Name",itemSetGet.getCourseName());
+                        hashMap.put("Request Date",currentdate+" Hrs");
+                        hashMap.put("Request Status","Unread");
+                        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference()
+                                .child("Requests").push();
+                        databaseReference.setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                DatabaseReference stdStatus=FirebaseDatabase.getInstance().getReference().child("Students")
+                                        .child("userId ");
+                                stdStatus.child("Status").setValue("Applied").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        course_description_dialog.dismiss();
+                                        Toast.makeText(Dashboard.this, "Success!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             }
         });
     }
